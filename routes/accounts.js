ad2c0bf5b081ab83da0810ab7a7cbe8a4478db97
chasoman <chasoman@in.ibm.com>
2018-04-25 06:15:23 +0000

    Created an accounts.js and linked it into the routes.js

---
+++ routes/accounts.js
@@ -0,0 +1,7 @@
+var accountsService = function(req, res) {
+    
+    res.send("Hi this is the accounts service");
+
+}
+
+module.exports = accountsService;
--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,10 @@ var appRouter = function(app) {
     app.get("/", function(req, res) {
         res.send("Hello World");
     });
-
+	
+	app.get("/accounts", function(req, res){
+		require("./accounts.js")(req, res);
+	});
 }
 
 module.exports = appRouter;
