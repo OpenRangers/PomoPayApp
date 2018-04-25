f6adee437666095f4c66f5aaa2adef11a6a6baa5
chasoman <soman.chaitanya@gmail.com>
2018-04-25 14:10:17 +0000

    Fixed the logic to get cloudant credentials from env

--- routes/customer.js
+++ routes/customer.js
@@ -2,12 +2,12 @@ var postCustomer = function(req, res) {
     
     // Parse the VCAP Environment to get the Cloudant URL
     var vcap_env = JSON.parse(process.env.VCAP_SERVICES);
-    var cloudant_url = vcap_env['cloudantNoSQLDB'][0]['url'];
-    console.log("The Cloudant URL is : %s",cloudant_url);
+    var cloudant_credentials = vcap_env['cloudantNoSQLDB'][0]['credentials'];
+    console.log("The Cloudant URL is : ",cloudant_credentials.url);
     
     // Connect to the pomopaycustomers DB
     var Cloudant = require('@cloudant/cloudant');
-    var cloudant = Cloudant({url: cloudant_url});
+    var cloudant = Cloudant({url: cloudant_credentials.url});
     var pomopaycustomersdb = cloudant.db.use('pomopaycustomers');
     //  insert the incoming data into the DB
     var idValue = pomopaycustomersdb.insert(req.body, function(err, data) {
