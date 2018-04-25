14acb43f377f38122cba9f18269c77c87e0a3684
chasoman <chasoman@in.ibm.com>
2018-04-25 04:40:08 +0000

    Updated server.js and removed the routes variable assignment as it is not required

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-var routes = require("./routes/routes.js")(app);
+require("./routes/routes.js")(app);
 
 var server = app.listen(8080, function() {
     console.log("Listening on port %s...", server.address().port);
