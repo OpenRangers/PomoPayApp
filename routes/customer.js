0084e99cbcc0e43c592f4aa110f20f4ff58bbff0
chasoman <chasoman@in.ibm.com>
2018-04-26 05:49:02 +0000

    Updated the error and data reporting mechanisms

--- routes/customer.js
+++ routes/customer.js
@@ -1,9 +1,6 @@
 var postCustomer = function(req, res) {
     
     
-    var cloudantDoc = null; // Define a variable to hold the coudant return document.
-    var cloudantErr = null; //Define a variable to hold the cloudant error.
-    
     // Parse the VCAP Environment to get the Cloudant URL
     var vcap_env = JSON.parse(process.env.VCAP_SERVICES);
     var cloudant_credentials = vcap_env['cloudantNoSQLDB'][0]['credentials'];
@@ -17,23 +14,15 @@ var postCustomer = function(req, res) {
     //  insert the incoming data into the DB
     pomopaycustomersdb.insert(req.body, function(err, data) {
     
-    cloudantDoc = data; //store the cloudant return document.
-    cloudantErr = err; // store the cloudant return error.
-
-    console.log('Error:', cloudantErr);
-    console.log('Data:', cloudantDoc);
-    
+ 	if(err){
+ 		res.send(err, 500);
+ 	}else{
+ 		res.send(data, 200);
+ 	}
+ 	
+	return;
+	
     });
-    
-    if(cloudantErr === null){
-		res.status(200);
-		res.contentType("application/json");
-    	res.send({"status":"SUCCESS","id":cloudantDoc.body.id,"version":cloudantDoc.body.ver});
-    }else{
-		res.status(404);
-		res.contentType("application/json");
-    	res.send({"status":"FAILURE","error":cloudantErr.message});	
-    }
 };
 
 var getCustomer = function(req, res){
@@ -52,7 +41,7 @@ var getCustomer = function(req, res){
 	pomopaycustomersdb.get(req.params.username, function(err, data) {
 
  	if(err){
- 		res.send(err.message, err['status-code']);
+ 		res.send(err, 500);
  	}else{
  		res.send(data, 200);
  	}
