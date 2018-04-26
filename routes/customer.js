f801022840ba85a75cda1369b15aa9d408551f53
chasoman <chasoman@in.ibm.com>
2018-04-26 05:41:26 +0000

    Updated the error and response reporting mechanisms

--- routes/customer.js
+++ routes/customer.js
@@ -34,13 +34,10 @@ var postCustomer = function(req, res) {
 		res.contentType("application/json");
     	res.send({"status":"FAILURE","error":cloudantErr.message});	
     }
-}
+};
 
 var getCustomer = function(req, res){
 
-    var cloudantDoc = null; // Define a variable to hold the coudant return document.
-    var cloudantErr = null; //Define a variable to hold the cloudant error.
-    
     // Parse the VCAP Environment to get the Cloudant URL
     var vcap_env = JSON.parse(process.env.VCAP_SERVICES);
     var cloudant_credentials = vcap_env['cloudantNoSQLDB'][0]['credentials'];
@@ -53,25 +50,17 @@ var getCustomer = function(req, res){
 	
 	// Read the document from the database
 	pomopaycustomersdb.get(req.params.username, function(err, data) {
- 
-    cloudantDoc = data; //store the cloudant return document.
-    cloudantErr = err; // store the cloudant return error.
-    
-    console.log('Error:', cloudantErr);
-    console.log('Data:', cloudantDoc);
-   
-	});
+
+ 	if(err){
+ 		res.send(err.message, err['status-code']);
+ 	}else{
+ 		res.send(data, 200);
+ 	}
+ 	
+	return;
 	
-	if(cloudantErr === null){
-		res.status(200);
-		res.contentType("application/json");
-		res.send({"status":"SUCCESS","id":cloudantDoc.body.id, "ver":cloudantDoc.body.ver});
-	}else{
-		res.status(404);
-		res.contentType("application/json");
-    	res.send({"status":"FAILURE","error":cloudantErr});	
-    }
-}
+	});
+};
 
 exports.postCustomer = postCustomer;
 exports.getCustomer = getCustomer;
