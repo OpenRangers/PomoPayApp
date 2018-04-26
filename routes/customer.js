71a2a4473437446f8138979a0315a6098e9ed702
chasoman <chasoman@in.ibm.com>
2018-04-26 00:05:00 +0000

    Updated the routes.js to add a path parameter Updated the customers.js to send JSON responses and parse path parameters

--- routes/customer.js
+++ routes/customer.js
@@ -1,5 +1,9 @@
 var postCustomer = function(req, res) {
     
+    
+    var cloudantDoc = null; // Define a variable to hold the coudant return document.
+    var cloudantErr = null; //Define a variable to hold the cloudant error.
+    
     // Parse the VCAP Environment to get the Cloudant URL
     var vcap_env = JSON.parse(process.env.VCAP_SERVICES);
     var cloudant_credentials = vcap_env['cloudantNoSQLDB'][0]['credentials'];
@@ -9,19 +13,55 @@ var postCustomer = function(req, res) {
     var Cloudant = require('@cloudant/cloudant');
     var cloudant = Cloudant({url: cloudant_credentials.url});
     var pomopaycustomersdb = cloudant.db.use('pomopaycustomers');
+    
     //  insert the incoming data into the DB
-    var idValue = pomopaycustomersdb.insert(req.body, function(err, data) {
+    pomopaycustomersdb.insert(req.body, function(err, data) {
     console.log('Error:', err);
     console.log('Data:', data);
+    
+    cloudantDoc = data; //store the cloudant return document.
+    cloudantErr = err; // store the cloudant return error.
+    
     });
     
-    
-    
-    res.send("Document created with _id"+idValue);
+    if(cloudantErr === null){
+    	res.send({"status":"SUCCESS","id":cloudantDoc._id,"version":cloudantDoc._ver});
+    }else{
+    	res.send({"status":"FAILURE","error":cloudantErr});	
+    }
 }
 
 var getCustomer = function(req, res){
-	res.send("Hi this is the GET customer service");
+
+    var cloudantDoc = null; // Define a variable to hold the coudant return document.
+    var cloudantErr = null; //Define a variable to hold the cloudant error.
+    
+    // Parse the VCAP Environment to get the Cloudant URL
+    var vcap_env = JSON.parse(process.env.VCAP_SERVICES);
+    var cloudant_credentials = vcap_env['cloudantNoSQLDB'][0]['credentials'];
+    console.log("The Cloudant URL is : ",cloudant_credentials.url);
+    
+    // Connect to the pomopaycustomers DB
+    var Cloudant = require('@cloudant/cloudant');
+    var cloudant = Cloudant({url: cloudant_credentials.url});
+    var pomopaycustomersdb = cloudant.db.use('pomopaycustomers');
+	
+	// Read the document from the database
+	pomopaycustomersdb.get(req.params.username, function(err, data) {
+    console.log('Error:', err);
+    console.log('Data:', data);
+
+    cloudantDoc = data; //store the cloudant return document.
+    cloudantErr = err; // store the cloudant return error.
+    
+    
+	});
+	
+	if(cloudantErr === null){
+		res.send({"status":"SUCCESS","document":{cloudantDoc}});
+	}else{
+    	res.send({"status":"FAILURE","error":cloudantErr});	
+    }
 }
 
 exports.postCustomer = postCustomer;
--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,15 @@
 var appRouter = function(app) {
     
-    app.get("/", function(req, res) {
+    app.get('/', function(req, res) {
         res.send("Hello World");
     });
 	
-	app.post("/customer", function(req, res){
+	app.post('/customer', function(req, res){
 		var customer = require("./customer.js");
 		customer.postCustomer(req, res);
 	});
 	
-	app.get("/customer", function(req, res){
+	app.get('/customer/:username', function(req, res){
 		var customer = require("./customer.js");
 		customer.getCustomer(req,res);
 	});
