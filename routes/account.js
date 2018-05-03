05c19c9b9a35896c5f54376bdc7f3786e4b0a56f
Mallika Paul <mallika.arpita.paul@gmail.com>
2018-05-03 16:00:41 +0000

    update

--- routes/account.js
+++ routes/account.js
@@ -15,7 +15,9 @@ var vefifyUsernamePwd = function(req, res) {
 	pomopaycustomersdb.get(req.params.username, function(err, data) {
 
  	if(err){
+ 		res.send(3);
  		res.send(err, 500);
+ 		
  	}else{
  		
  		var obj = JSON.parse(data);
