3cfda96ff3747dd51f82c1c90496cde25fff0be0
Mallika Paul <mallika.arpita.paul@gmail.com>
2018-05-03 10:22:18 +0000

    updating verify username and pwd

--- routes/account.js
+++ routes/account.js
@@ -1,3 +1,32 @@
 var vefifyUsernamePwd = function(req, res) {
+	
+	
+	var vcap_env = JSON.parse(process.env.VCAP_SERVICES);
+    var cloudant_credentials = vcap_env['cloudantNoSQLDB'][0]['credentials'];
+    console.log("The Cloudant URL is : ",cloudant_credentials.url);
+    
+    // Connect to the pomopaycustomers DB
+    var Cloudant = require('@cloudant/cloudant');
+    var cloudant = Cloudant({url: cloudant_credentials.url});
+    var pomopaycustomersdb = cloudant.db.use('pomopaycustomers');
+	
+	// Read the document from the database
+	pomopaycustomersdb.get(req.params.username, function(err, data) {
+
+ 	if(err){
+ 		res.send(err, 500);
+ 	}else{
+ 		var obj = JSON.parse(data);
+ 		if (obj.password==req.params.password)
+ 		{
+ 			res.send("match");
+ 		}
+ 		else
+ 		res.send("no match");
+ 	}
+ 	
+	return;
+	
+	});
 	};
 exports.vefifyUsernamePwd = vefifyUsernamePwd;
