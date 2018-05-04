d2dd40d2138931831a6e84f0ecde68b1d51ff870
Mallika Paul <mallika.arpita.paul@gmail.com>
2018-05-04 14:54:26 +0000

    update

--- routes/account.js
+++ routes/account.js
@@ -34,10 +34,10 @@ var getAccountList =  function(req, res) {
  		
  	}
  	
-	
+	res.send(accountlist);
 	
 	});
-	res.send(accountlist);
+	
  	/*for (var j in accountlist)	{
 	pomopayaccountdb.get(accountlist[j], function(accerr, accdata) {
  			if(accerr){
